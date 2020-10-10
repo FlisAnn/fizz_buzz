c0f2bcb1be07bb2ab84af0db1338561e4aa7df92
flis_ann <ann.flismark@gmail.com>
2020-10-10 16:22:24 +0200

    created first test and a readme file with the user stories

--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -3,5 +3,7 @@ require './lib/fizz_buzz'
 #  in the lib folder called fizz_buzz.rb
 
 describe 'fizz_buzz' do # test goes here
-
+it 'returns 1 if number is 1' do
+    expect(fizz_buzz(1)).to eq 1
+end
 end
