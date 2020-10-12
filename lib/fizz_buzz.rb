e11db3da6082d9b4cc64a9c1548bc01b089e69bc
flis_ann <ann.flismark@gmail.com>
2020-10-12 13:50:21 +0200

    asldkfj

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,11 +1,14 @@
-def fizz_buzz(number)
-    number
-end
+# The FizzBuzz program
+def fizz_buzz(number)  
+        if number % 5 == 0 && number % 3 == 0 # if number is dividable by 3 AND 5. Has to be first otherwise it wont be run
+            'fizzbuzz'
+        elsif number % 3 == 0 
+            'fizz'
+        elsif number % 5 == 0
+            'buzz'
+        else
+            number
+        end
+end 
 
-def fizz_buzz(number)
-    if number % 3 == 0
-        'fizz'
-    else
-        number
-    end
-end
+
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -4,11 +4,24 @@ require './lib/fizz_buzz'
 
 describe 'fizz_buzz' do # test goes here
 it 'returns 1 if number is 1' do
-    expect(fizz_buzz(1)).to eq 1
+    expect(fizz_buzz(1)).to eq 1 #
+    end
 end
-end
+
 describe 'fizz_buzz' do
-it "returns 'fizz' if number is divisible by 3" do 
-    expect(fizz_buzz(3)).to eq 'fizz'
+    it "returns 'fizz' if number is divisible by 3" do # checks 3
+    expect(fizz_buzz(3)).to eq 'fizz' 
+    end
 end
+
+describe 'fizz_buzz' do
+    it "returns 'buzz' if number is divisible by 5" do # checks 5
+        expect(fizz_buzz(5)).to eq 'buzz'
+    end
 end
+
+describe 'fizz_buzz' do
+    it "returns 'fizzbuzz' if number is divisible by 3 and 5" do #wrong? 
+        expect(fizz_buzz(15)).to eq 'fizzbuzz'
+        end
+    end
